d49c987223d844dbc256eb03e0bca35121547775
Alejandro Fontal <alejandro.fontal.92@gmail.com>
2025-04-02 14:52:33 +0200

    feat: add project structure and initial data files

---
+++ {{ project_name }}/code/src.py
@@ -0,0 +1,9 @@
+# This is a placeholder file for the {{ project_name }} project.
+# You can use this file to add your source code for data processing, analysis, or modeling.
+
+def example_function():
+    """
+    Example function to demonstrate how to structure your code.
+    Replace this with your actual implementation.
+    """
+    return "This is a placeholder function. Replace it with your code."
